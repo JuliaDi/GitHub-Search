8e8a238da8089c9864daaacc0e271e0a67c61df6
Julia Di <di.julia.8@gmail.com>
2015-02-08 12:24:39 -0500

    Added comments

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# *************************
+# Julia Di
+# February 5, 2014
+# This was a project to familiarize myself with using APIs for
+# my first hacakthon. It isn't quite complete yet.
+# *************************
+
 from flask import Flask, jsonify, render_template, request
 import requests
 
@@ -6,10 +13,11 @@ app = Flask(__name__)
 @app.route("/search", methods=["GET", "POST"])
 def search():
     if request.method == "POST":
-        url = "https://api.github.com/search/repositories?q=" + request.form["user_search"]
+        url = "https://api.github.com/search/repositories?q=" + 
+              request.form["user_search"]
         response_dict = requests.get(url).json()
         return render_template("results.html", api_data=response_dict)
-    else: # request.method == "GET"
+    else: # when request.method == "GET"
         return render_template("search.html")
 
 @app.errorhandler(404)
